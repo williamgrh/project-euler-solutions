9780ecb7d23ab2cc47c31ebe71a8dd99c4d2d6b9
William Rigby-Hall <williamgrh@gmail.com>
2016-02-02 17:11:41 -0500

    More solutions

---
+++ Python/0009.py
@@ -0,0 +1,24 @@
+#
+# 0009 - Special Pythagorean triplet
+#
+# https://projecteuler.net/problem=9
+# https://github.com/williamgrh/ProjectEulerSolutions
+#
+
+def compute():
+    for c in range(1000):
+        for b in range(c):
+            for a in range (b):
+                if a + b + c == 1000 and check_triplet(a, b, c):
+                    return (a * b * c)
+    
+    
+def check_triplet(a, b, c):
+    if ( a**2 + b**2 == c**2):
+        return True
+    else:
+        return False
+    
+
+if __name__ == "__main__":
+    print ( str ( compute() ) )
---
+++ Python/0010.py
@@ -0,0 +1,16 @@
+#
+# 0010 - Summation of primes
+#
+# https://projecteuler.net/problem=10
+# https://github.com/williamgrh/ProjectEulerSolutions
+#
+
+import eulerlib
+
+def compute():
+    ans = sum(eulerlib.list_primes(2000000))
+    return ans
+
+
+if __name__ == "__main__":
+    print ( str ( compute() ) )
